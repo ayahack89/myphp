1efbb5655ff38b3ef8a2d642486427ae89efb3e8
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-03-09 15:13:51 +0530

    Update

---
+++ project/udagchat/comp/_footer.php
@@ -0,0 +1,30 @@
+
+
+
+<footer class="p-4 shadow md:px-6 md:py-8">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <a href="https://www.thunderdevelops.in/sendbox/" class="flex items-center mb-4 sm:mb-0">
+            <img src="php/images/sendbox_logo.svg" class="mr-3 h-8" alt="Logo" />
+            <span class="self-center text-sm font-bold whitespace-nowrap items-center text-gray-900">Powered by Thunder Develops</span>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 text-sm  sm:mb-0 text-gray-500">
+        <li>
+                <a href="https://www.thunderdevelops.in/about-us.php" class="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+                <a href="https://www.thunderdevelops.in/contact-us.php" class="mr-4 hover:underline md:mr-6 ">Contact</a>
+            </li>
+            <li>
+                <a href="https://www.thunderdevelops.in/privacy-policy.php" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            </li>
+        </ul>
+    </div>
+    <hr class="my-6  sm:mx-auto border-gray-700 lg:my-8" />
+    <span class="block text-sm  sm:text-center text-gray-400">© <?php date("Y"); ?> <a href="https://www.thunderdevelops.in/sendbox/" class="hover:underline text-cyan-800 font-bold">SendBox</a>. All Rights Reserved.</span>
+    <span class="block text-sm sm:text-center text-gray-400">
+        Design & Developed by
+        <a href="https://www.instagram.com/krishpanchani/" 
+    target="_blank" title="Krish Panchani | Instagram" class="hover:underline text-gray-800 font-bold"> Krish Panchani </a> And Powerd by 
+    <a href="https://www.thunderdevelops.in/" target="_blank" title="Thunder Develops | Official Website" class="hover:underline text-gray-800 font-bold">Thunder Develops. </a>
+    </span>
+</footer>
