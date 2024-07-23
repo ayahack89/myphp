3ff0c604817bde3a1dbee3210dc979d02c451a8a
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-07-23 23:28:57 +0530

    file updates

--- Elocker/cdn.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-<link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
