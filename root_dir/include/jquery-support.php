01f242daebf0fc45418ecb89c9b44ec760579962
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2025-04-07 00:56:41 +0530

    Update

--- root_dir/include/jquery-support.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.3/themes/base/jquery-ui.css">
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://code.jquery.com/ui/1.13.3/jquery-ui.min.js"></script>
--- root_dir/user_account/settings/asset/script.php
+++
@@ -1,3 +0,0 @@
-<!-- scripts.php -->
-<script src="../fontawesome-free-6.7.2-web/js/all.min.js"></script>
-<script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
